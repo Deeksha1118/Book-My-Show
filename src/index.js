2458323a85371d2652f3b1947f80269b956913c7
Deeksha1118 <164308667+Deeksha1118@users.noreply.github.com>
2025-02-09 01:04:47 +0530

    Project Completed

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import MovieProvider from './Context/MovieContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Book-My-Show">
       <MovieProvider>
         <App />
       </MovieProvider>
